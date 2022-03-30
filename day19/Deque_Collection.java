9cbb37766ea119a9ba17675d642ca029c39f5b45
RameesJabbar <101250579+RameesJabbar@users.noreply.github.com>
2022-03-30 18:01:02 +0530

    Add files via upload

---
+++ day19/Deque_Collection.java
@@ -0,0 +1,20 @@
+package day19;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Deque_Collection {
+
+	public static void main(String[] args) {
+		//Creating Deque and adding elements  
+		Deque<String> deque = new ArrayDeque<String>();  
+		deque.add("Gautam");  
+		deque.add("Karan");  
+		deque.add("Ajay");  
+		//Traversing elements  
+		for (String str : deque) {  
+		System.out.println(str);  
+		}  
+	}
+
+}
